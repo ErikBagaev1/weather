51e852db3ccff36ac47417d0cc63fba84ded93c7
Ergos <bagaeff.erik@yandex.ru>
2023-11-12 16:47:07 +0300

    Учебное приложение по использованию API

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:weather/model/weather_model.dart';
+import 'package:weather/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService =
+      WetaherService(apiKey: 'c0c61605889d068580394c669555cd82');
+  Weather? _weather;
+
+  _feachWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _feachWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? 'Загрузка погоды'),
+            Text('${_weather?.temperature.round()} C')
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/weather_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather/model/weather_model.dart';
+
+class WetaherService {
+  // ignore: constant_identifier_names
+  static const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+  final String apiKey;
+
+  WetaherService({required this.apiKey});
+
+  Future<Weather> getWeather(String cityName) async {
+    final parse = '$BASE_URL?q=$cityName&appid=$apiKey&units=metric';
+    final response = await http.get(Uri.parse(parse));
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Ошибка загрузки погоды');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    List<Placemark> placemark =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    String? city = placemark[0].locality;
+    return city ?? ' ';
+  }
+}
